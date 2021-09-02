6a7ebbfb8e2f07f0539f6446ef63a80e8e2d65dd
Jacob Bieker <jacob@openclimatefix.org>
2021-09-02 13:01:46 +0100

    Start adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="metnet",
+    version="0.0.1",
+    packages=["metnet"],
+    url="https://github.com/openclimatefix/metnet",
+    license="MIT License",
+    company="Open Climate Fix Ltd",
+    author="Jacob Bieker",
+    author_email="jacob@openclimatefix.org",
+    description="PyTorch MetNet Implementation",
+)
