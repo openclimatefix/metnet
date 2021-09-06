fb60e7eefcef79fa10e0f9895e3663cafa61d61a
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 14:08:18 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="metnet",
-    version="0.0.3",
+    version="0.0.4",
     packages=["metnet"],
     url="https://github.com/openclimatefix/metnet",
     license="MIT License",
