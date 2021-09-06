505f0d42c8b3e48b574a153cbd1e46978745ef1d
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 13:19:28 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name="metnet",
-    version="0.0.1",
+    version="0.0.2",
     packages=["metnet"],
     url="https://github.com/openclimatefix/metnet",
     license="MIT License",
@@ -10,4 +12,21 @@ setup(
     author="Jacob Bieker",
     author_email="jacob@openclimatefix.org",
     description="PyTorch MetNet Implementation",
+    keywords=[
+        "artificial intelligence",
+        "deep learning",
+        "transformer",
+        "attention mechanism",
+        "metnet",
+        "forecasting",
+        "remote-sensing"
+    ],
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
