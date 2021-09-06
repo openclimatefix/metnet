bfe8f4a807bad7dbdd1d41f0e14afc589e2689ae
Jacob Bieker <jacob@openclimatefix.org>
2021-09-06 10:05:22 +0100

    Add tests

---
+++ metnet/__init__.py
@@ -0,0 +1,1 @@
+from .metnet import MetNet
---
+++ tests/test_model.py
@@ -0,0 +1,22 @@
+import torch
+from metnet import MetNet
+
+
+def test_metnet_creation():
+    model = MetNet(hidden_dim=32, forecast_steps=24, input_channels=16, output_channels=12, sat_channels=12, input_size=64)
+    # MetNet expects original HxW to be 4x the input size
+    x = torch.randn(
+        (2, 12, 16, 256, 256)
+    )
+    model.eval()
+    with torch.no_grad():
+        out = model(x)
+    # MetNet creates predictions for the center 1/4th
+    assert out.size() == (
+        2,
+        24,
+        12,
+        16,
+        16,
+    )
+    assert not torch.isnan(out).any(), "Output included NaNs"
