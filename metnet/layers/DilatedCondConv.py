4864dcc70e615df10dfa21e1d8deccee6a664744
Jacob Bieker <jacob@bieker.tech>
2022-01-25 15:08:15 +0000

    Add type hints

--- metnet/layers/DilatedCondConv.py
+++ metnet/layers/DilatedCondConv.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 class DilatedResidualConv(nn.Module):
-    def __init__(self, input_channels, output_channels, dilation, kernel_size, activation=nn.ReLU()):
+    def __init__(self, input_channels: int, output_channels: int = 384, dilation: int = 1, kernel_size: int = 3, activation: nn.Module = nn.ReLU()):
         super().__init__()
         self.dilated_conv_one = nn.Conv2d(in_channels=input_channels, out_channels=output_channels, dilation=dilation, kernel_size=kernel_size)
         # TODO Pass in the non-batch size things for layer norm
@@ -15,7 +15,7 @@ class DilatedResidualConv(nn.Module):
         self.dilated_conv_two = nn.Conv2d(in_channels=output_channels, out_channels=output_channels, dilation=dilation, kernel_size=kernel_size)
         self.layer_norm_two = nn.LayerNorm(normalized_shape=(output_channels,))
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         out = self.dilated_conv_one(x)
         out = self.layer_norm_one(out)
         # TODO Add target time conditioning
