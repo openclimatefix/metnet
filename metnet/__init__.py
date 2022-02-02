5c72991abfc6a231a76c38ac8bf892a201d5825c
Jacob Bieker <jacob@bieker.tech>
2022-02-02 12:09:55 +0000

    Add tests and fix import

--- metnet/__init__.py
+++ metnet/__init__.py
@@ -2,4 +2,3 @@ from metnet.models.metnet import MetNet
 from metnet.models.metnet2 import MetNet2
 
 from .layers import *
-from .model import MetNet
---
+++ metnet/models/__init__.py
@@ -0,0 +1,2 @@
+from .metnet import MetNet
+from .metnet2 import MetNet2
