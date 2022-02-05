a13bcf76ed042e9b052b05f63cedb81dbb59a62b
Jacob Bieker <jacob@bieker.tech>
2022-02-05 10:12:17 +0000

    Add backward tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 
 from metnet import MetNet, MetNet2
 
@@ -25,6 +26,30 @@ def test_metnet_creation():
         16,
         16,
     )
+    assert not torch.isnan(out).any(), "Output included NaNs"
+
+def test_metnet_backwards():
+    model = MetNet(
+        hidden_dim=32,
+        forecast_steps=24,
+        input_channels=16,
+        output_channels=12,
+        sat_channels=12,
+        input_size=32,
+        )
+    # MetNet expects original HxW to be 4x the input size
+    x = torch.randn((2, 12, 16, 128, 128))
+    out = model(x)
+    # MetNet creates predictions for the center 1/4th
+    assert out.size() == (
+        2,
+        24,
+        12,
+        8,
+        8,
+        )
+    y = torch.randn((2, 24, 12, 8, 8))
+    F.mse_loss(out, y).backward()
     assert not torch.isnan(out).any(), "Output included NaNs"
 
 
@@ -59,4 +84,29 @@ def test_metnet2_creation():
         128,
         128,
     )
+    assert not torch.isnan(out).any(), "Output included NaNs"
+
+def test_metnet2_backward():
+    model = MetNet2(
+        forecast_steps=8,
+        input_size=64,
+        num_input_timesteps=6,
+        upsampler_channels=128,
+        lstm_channels=32,
+        encoder_channels=64,
+        center_crop_size=16,
+        )
+    # MetNet expects original HxW to be 4x the input size
+    x = torch.randn((2, 6, 12, 256, 256))
+    out = model(x)
+    # MetNet creates predictions for the center 1/4th
+    assert out.size() == (
+        2,
+        8,
+        12,
+        64,
+        64,
+        )
+    y = torch.rand((2,8,12,64,64))
+    F.mse_loss(out, y).backward()
     assert not torch.isnan(out).any(), "Output included NaNs"
