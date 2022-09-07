49e8bd656e4fdfe5f21ed48184a5b625c0351f5d
BumpVersion Action <bumpversion@github-actions>
2022-09-07 15:19:41 +0000

    Bump version: 4.0.1 → 4.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="metnet",
-    version="4.0.1",
+    version="4.0.2",
     packages=find_packages(),
     url="https://github.com/openclimatefix/metnet",
     license="MIT License",
