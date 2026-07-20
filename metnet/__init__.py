9ca13d4982d4bc07285e53a2d67d229a88025bce
Tantrik <32596643+trudramukerji14@users.noreply.github.com>
2026-07-20 15:20:12 -0500

    Feature/metnet3 model (#83)
    * Initial commit to add MetNet3 Embedding
    
    * ruff format
    
    * ruff docstring formatting
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add metnet3 model
    
    * add device to test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- metnet/__init__.py
+++ metnet/__init__.py
@@ -2,6 +2,7 @@
 
 from metnet.models.metnet import MetNet
 from metnet.models.metnet2 import MetNet2
+from metnet.models.metnet3 import MetNet3
 from metnet.models.metnet_pv import MetNetPV
 
 from .layers import (
